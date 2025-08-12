3fbcb9b31378ae4cc819428587fe5c2d7f3ac823
TEMITECH-BOY <temitayoosunla@gmail.com>
2025-08-12 22:55:23 +0100

    Added root URL welcome page

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,9 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
 from users.views import RegisterView
 
+def home(request):
+    return HttpResponse("Welcome to the Real Estate API")
+
 urlpatterns = [
+    path('', home),  # ✅ Root URL handler
+
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('listings.urls')),
@@ -19,4 +25,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
